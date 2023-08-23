2065ef72f5f2dad6ddde90a57ec338861230afc6
John Wang <johnz.wang@outlook.com>
2023-08-23 17:55:55 +0200

    I hate nano

---
+++ flake.nix
@@ -0,0 +1,22 @@
+# /etc/nixos/flake.nix
+{
+  description = "flake for yourHostNameGoesHere";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      idea = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ../nix/idea.nix
+        ];
+      };
+    };
+  };
+}
+
