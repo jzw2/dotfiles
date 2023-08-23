7f603efb48538b91cfdbe9110e55d36505e52301
John Wang <johnz.wang@outlook.com>
2023-08-23 21:17:00 +0200

    finally made it start

--- nix/idea/idea.nix
+++ nix/idea/idea.nix
@@ -83,8 +83,10 @@
     };
   };
   # Enable the GNOME 3 Desktop Environment.
+  services.xserver.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.debug = true;
   
 
   # Configure keymap in X11
