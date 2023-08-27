1066e2fae3adaa13e0a9006143f511b1c9e77f80
John Wang <johnz.wang@outlook.com>
2023-08-27 20:21:44 +0200

    Add flatpak

--- nix/idea/idea.nix
+++ nix/idea/idea.nix
@@ -161,7 +161,7 @@
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
   services.emacs.enable = true;
-
+  services.flatpak.enable = true;
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
