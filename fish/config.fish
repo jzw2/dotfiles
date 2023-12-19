05d52f1efb5d364aeca59a18b45592e57f460adb
John Wang <johnz.wang@outlook.com>
2023-12-19 11:27:10 +0100

    Actually make it work

--- fish/config.fish
+++ fish/config.fish
@@ -8,13 +8,13 @@ abbr -a org hx ~/org
 
 function today
     set today (date "+%Y-%m-%d")
-    cd /home/john/org/roam/daily
+    cd /home/john/org/
     if test -e "roam/daily/$today.org"
         hx "roam/daily/$today.org"
     else
         echo "Creating file for today: $today.org"
         touch "roam/daily/$today.org"
-        echo "#+TITLE: $today\n\n* Journal\n\n* Tasks" > "roam/daily/$today.org"
+        echo -e "#+TITLE: $today\n\n* Journal\n\n* Tasks" > "roam/daily/$today.org"
         hx "roam/daily/$today.org"
     end
 end
