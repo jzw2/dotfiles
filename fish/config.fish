a3b943a84f10a88e52576bdae09b698a25addfa5
John Wang <johnz.wang@outlook.com>
2023-12-19 10:38:13 +0100

    Modify fish daily

--- fish/config.fish
+++ fish/config.fish
@@ -5,15 +5,16 @@ end
 abbr -a todo habitipy todos add 
 abbr -a org hx ~/org
 
+
 function today
     set today (date "+%Y-%m-%d")
     cd /home/john/org/roam/daily
-    if test -e "$today.org"
-        hx "$today.org"
+    if test -e "roam/daily/$today.org"
+        hx "roam/daily/$today.org"
     else
         echo "Creating file for today: $today.org"
-        touch "$today.org"
-        hx "$today.org"
+        touch "roam/daily/$today.org"
+        echo "#+TITLE: $today\n\n* Journal\n\n* Tasks" > "roam/daily/$today.org"
+        hx "roam/daily/$today.org"
     end
 end
-
