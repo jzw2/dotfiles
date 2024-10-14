341f32ba24349ec76d8d1e7a26752aea756e284b
John Wang <johnz.wang@outlook.com>
2024-10-14 14:38:36 +0200

    Add yazi fish completion

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,14 @@ end
 abbr -a todo habitipy todos add 
 abbr -a org hx ~/org
 
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
 
 function today
     set today (date "+%Y-%m-%d")
