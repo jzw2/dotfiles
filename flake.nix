c15c4169d4067ffa42e7fe9425c82065b105be50
John Wang <johnz.wang@outlook.com>
2025-01-05 13:55:22 +0100

    Add substituter

--- flake.nix
+++ flake.nix
@@ -23,8 +23,8 @@
         modules = [ 
           {
             nix.settings = {
-              substituters = [ "https://cosmic.cachix.org/" ];
-              trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+              substituters = [ "https://cosmic.cachix.org/" "https://devenv.cachix.org" ];
+              trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" ];
             };
           }
           nixos-cosmic.nixosModules.default
