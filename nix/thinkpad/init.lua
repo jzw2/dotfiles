4a5ef664d105d47c76ada08f0abf1ceef7ac495b
John Wang <johnz.wang@outlook.com>
2026-07-18 22:26:35 +0200

    Fix to have lsp in init

--- nix/thinkpad/init.lua
+++ nix/thinkpad/init.lua
@@ -4,7 +4,6 @@ vim.g.python3_host_prog = "/run/current-system/sw/bin/python3"
 
 vim.g.have_nerd_font = true
 
-
 vim.cmd.colorscheme "industry"
 
 vim.opt.number = true
@@ -16,57 +15,114 @@ vim.opt.mouse = 'a'
 vim.opt.showmode = false
 
 vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = -1
+
+
+
+
 
 local nvim_lsp = vim.lsp
 nvim_lsp.config("nixd", {
-        settings = {
-                nixd = {
-                        nixpkgs = {
-                                expr = "import <nixpkgs> { }",
-                        },
-                        formatting = {
-                                command = { "nixfmt" },
-                        },
-                        options = {
-                                nixos = {
-                                        expr =
-                                        '(builtins.getFlake (toString ./.)).nixosConfigurations.<hostname>.options',
-                                },
-                        },
-                },
+  settings = {
+    nixd = {
+      nixpkgs = {
+        expr = "import <nixpkgs> { }",
+      },
+      formatting = {
+        command = { "nixfmt" },
+      },
+      options = {
+        nixos = {
+          expr =
+          '(builtins.getFlake (toString ./.)).nixosConfigurations.<hostname>.options',
         },
+      },
+    },
+  },
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('my.lsp', {}),
-        callback = function(ev)
-                local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
-                if client:supports_method('textDocument/implementation') then
-                        -- Create a keymap for vim.lsp.buf.implementation ...
-                end
+  group = vim.api.nvim_create_augroup('my.lsp', {}),
+  callback = function(ev)
+    local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+    if client:supports_method('textDocument/implementation') then
+      -- Create a keymap for vim.lsp.buf.implementation ...
+    end
 
-                -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
-                if client:supports_method('textDocument/completion') then
-                        -- Optional: trigger autocompletion on EVERY keypress. May be slow!
-                        -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-                        -- client.server_capabilities.completionProvider.triggerCharacters = chars
+    -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
+    if client:supports_method('textDocument/completion') then
+      -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+      -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+      -- client.server_capabilities.completionProvider.triggerCharacters = chars
 
-                        vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-                end
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
 
-                -- Auto-format ("lint") on save.
-                -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-                if not client:supports_method('textDocument/willSaveWaitUntil')
-                    and client:supports_method('textDocument/formatting') then
-                        vim.api.nvim_create_autocmd('BufWritePre', {
-                                group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
-                                buffer = ev.buf,
-                                callback = function()
-                                        vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
-                                end,
-                        })
-                end
+    -- Auto-format ("lint") on save.
+    -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
+    if not client:supports_method('textDocument/willSaveWaitUntil')
+        and client:supports_method('textDocument/formatting') then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+        buffer = ev.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
         end,
+      })
+    end
+  end,
+})
+
+vim.lsp.config('lua_ls', {
+  on_init = function(client)
+    -- if client.workspace_folders then
+    --   local path = client.workspace_folders[1].name
+    --   if
+    --       path ~= vim.fn.stdpath('config')
+    --       and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+    --   then
+    --     return
+    --   end
+    -- end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most
+        -- likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Tell the language server how to find Lua modules same way as Neovim
+        -- (see `:h lua-module-load`)
+        path = {
+          'lua/?.lua',
+          'lua/?/init.lua',
+          '?/init.lua',
+        },
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          -- For LSP Settings Type Annotations: https://github.com/neovim/nvim-lspconfig#lsp-settings-type-annotations
+          vim.api.nvim_get_runtime_file("lua/lspconfig", false)[1],
+          -- Depending on the usage, you might want to add additional paths
+          -- here.
+          -- '${3rd}/luv/library',
+          -- '${3rd}/busted/library',
+        },
+        -- Or pull in all of 'runtimepath'.
+        -- NOTE: this is a lot slower and will cause issues when working on
+        -- your own configuration.
+        -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+        -- library = vim.api.nvim_get_runtime_file('', true),
+      },
+    })
+  end,
+  settings = {
+    Lua = {},
+  },
+
 })
 
 vim.lsp.enable('tinymist')
